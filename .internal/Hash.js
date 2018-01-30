b7df4b7e4ccab62d232cd94c31749a65379f02c9
DrugsZ <1002881508@qq.com>
2018-01-30 13:56:15 +0800

    删除之前测试代码

--- .internal/Hash.js
+++ .internal/Hash.js
@@ -1,89 +1,37 @@
+/*
+ * @Author: DrugsZ 
+ * @Date: 2018-01-03 11:05:29 
+ * @Last Modified by: DrugsZ
+ * @Last Modified time: 2018-01-11 16:29:32
+ */
 /** Used to stand-in for `undefined` hash values. */
 const HASH_UNDEFINED = '__lodash_hash_undefined__'
 
-class Hash {
-
-  /**
-   * Creates a hash object.
-   *
-   * @private
-   * @constructor
-   * @param {Array} [entries] The key-value pairs to cache.
-   */
+class hash {
   constructor(entries) {
     let index = -1
-    const length = entries == null ? 0 : entries.length
-
+    const lenth = entries == null?0:entries.lenth
+    //??
     this.clear()
-    while (++index < length) {
-      const entry = entries[index]
-      this.set(entry[0], entry[1])
+    while(++index<length){
+      const entry = entries[index];
+      //??
+      this.set(entry[0],entry[1])
     }
   }
 
-  /**
-   * Removes all key-value entries from the hash.
-   *
-   * @memberOf Hash
-   */
-  clear() {
+  clear(){
     this.__data__ = Object.create(null)
     this.size = 0
   }
 
-  /**
-   * Removes `key` and its value from the hash.
-   *
-   * @memberOf Hash
-   * @param {Object} hash The hash to modify.
-   * @param {string} key The key of the value to remove.
-   * @returns {boolean} Returns `true` if the entry was removed, else `false`.
-   */
-  delete(key) {
-    const result = this.has(key) && delete this.__data__[key]
-    this.size -= result ? 1 : 0
+  delete(key){
+    const result = this.has(key) && delete this.__data__[key];
+    this.size -= result?1:0
     return result
   }
 
-  /**
-   * Gets the hash value for `key`.
-   *
-   * @memberOf Hash
-   * @param {string} key The key of the value to get.
-   * @returns {*} Returns the entry value.
-   */
-  get(key) {
+  get(key){
     const data = this.__data__
-    const result = data[key]
-    return result === HASH_UNDEFINED ? undefined : result
-  }
-
-  /**
-   * Checks if a hash value for `key` exists.
-   *
-   * @memberOf Hash
-   * @param {string} key The key of the entry to check.
-   * @returns {boolean} Returns `true` if an entry for `key` exists, else `false`.
-   */
-  has(key) {
-    const data = this.__data__
-    return data[key] !== undefined
-  }
-
-  /**
-   * Sets the hash `key` to `value`.
-   *
-   * @memberOf Hash
-   * @param {string} key The key of the value to set.
-   * @param {*} value The value to set.
-   * @returns {Object} Returns the hash instance.
-   */
-  set(key, value) {
-    const data = this.__data__
-    this.size += this.has(key) ? 0 : 1
-    data[key] = value === undefined ? HASH_UNDEFINED : value
-    return this
   }
 }
-
-export default Hash
