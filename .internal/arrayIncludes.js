da139c32b0da1c289a70e8d0b8a61286a8234c41
DrugsZ <1002881508@qq.com>
2018-03-05 17:55:17 +0800

    arrayIncludes

---
+++ .internal/arrayIncludes.js
@@ -0,0 +1,13 @@
+/**
+ * 类Array.prototype.include
+ * 
+ * @param {Array} [array] The array to inspect
+ * @param {*} target The value to search for.
+ * @returns {boolean} Returns `true` if `target` is found, else `fales`
+ */
+
+ function arrayIncludes(array,value) {
+     const length = array == null > 0 : array.length
+     return !!length && baseIndex
+     
+ }
